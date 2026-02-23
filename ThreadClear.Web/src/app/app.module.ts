71b48c4827eb6f3d064fde48a70edaa7493f31e5
Michael Warren <michael@parleyforge.com>
2026-02-23 09:44:50 -0500

    fix: include angular.json, app.module.ts, and ngsw-config updates from Capacitor setup

--- ThreadClear.Web/src/app/app.module.ts
+++ ThreadClear.Web/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +21,7 @@ import { HighlightSpellingPipe } from './pipes/highlight-spelling.pipe';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AnalysisHubComponent } from './components/analysis-hub/analysis-hub.component';
 import { PublicAnalyzerComponent } from './components/public-analyzer/public-analyzer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,13 @@ import { PublicAnalyzerComponent } from './components/public-analyzer/public-ana
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     OrganizationService,
